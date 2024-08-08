882bcf943087f522f3a9149b80ee8840e84ac7d0
JuanJoseMoraCarrascal <juanjosecarrascal@outlook.com>
2024-08-08 16:32:02 -0500

    Myfirstcomponent

---
+++ src/componentes/Myfirstcomponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Myfirstcomponent = () => {
+  return (
+    <div>
+      <div>Mi primer componente</div>
+      <div>Hello World, i am Juan Carrascal</div>
+      <div>27 de junio de 2024</div>
+    </div>
+  )
+}
+
+export default Myfirstcomponent
